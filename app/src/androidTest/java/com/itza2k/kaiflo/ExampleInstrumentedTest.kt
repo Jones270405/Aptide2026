b989924a6632a22d118c851d18022598434470f3
Joanna Satya <99137601+Jones270405@users.noreply.github.com>
2026-06-29 12:12:58 +0530

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/itza2k/kaiflo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/itza2k/kaiflo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.itza2k.kaiflo
+package com.Jones270405.Aptide 
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.itza2k.kaiflo", appContext.packageName)
+        assertEquals("com.Jones270405.Aptide", appContext.packageName)
     }
 }
