94d134953fb7b45b74b1af7a5f00528901e9c7f3
Joanna Satya <99137601+Jones270405@users.noreply.github.com>
2026-06-29 12:55:55 +0530

    Update MainActivity.kt

--- app/src/main/java/com/itza2k/kaiflo/MainActivity.kt
+++ app/src/main/java/com/itza2k/kaiflo/MainActivity.kt
@@ -1,13 +1,13 @@
-package com.itza2k.kaiflo
+package com.Jones270405.Aptide
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.lifecycle.ViewModelProvider
-import com.itza2k.kaiflo.ui.screen.SetupUI
-import com.itza2k.kaiflo.viewmodel.MainViewModel
-import com.itza2k.kaiflo.viewmodel.MainViewModelFactory
+import com.Jones270405.Aptide.ui.screen.SetupUI
+import com.Jones270405.Aptide.viewmodel.MainViewModel
+import com.Jones270405.Aptide.viewmodel.MainViewModelFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
